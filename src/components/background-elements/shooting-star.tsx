b2e5345473521d41465fdfb68eb8272a31f69ec6
GraydonvG <graydonvg@gmail.com>
2024-10-22 16:13:17 +0200

    calculate shooting star travel distance and animation duration, use intersection observer to remove star once it exits the viewport

--- src/components/background-elements/shooting-star.tsx
+++ src/components/background-elements/shooting-star.tsx
@@ -12,9 +12,10 @@ type ShootingStar = {
   tailHeight: string;
   tailWidth: string;
   travelDistance: string;
+  animationDuration: string;
 };
 
-function calculateRandomAngleTowardsCenter(x: number, y: number): string {
+function calculateRandomAngleTowardsCenter(x: number, y: number) {
   const viewportWidth = window.innerWidth;
   const viewportHeight = window.innerHeight;
 
@@ -32,36 +33,40 @@ function calculateRandomAngleTowardsCenter(x: number, y: number): string {
   const randomOffset = Math.random() * 160 - 80;
   // Ensure the angle is positive
   const nonNegativeAngle = angleDegrees + randomOffset + 360;
-  // Ensure the angle stays withing 0 - 359
+  // Ensure the angle stays withing 0 - 359 (at 360 the angle is 0 again)
   angleDegrees = nonNegativeAngle % 360;
 
-  return `${angleDegrees}deg`;
+  return angleDegrees;
 }
 
 function createShootingStar(): ShootingStar {
+  const top = Math.random() * window.innerHeight;
+  const left = Math.random() * window.innerWidth;
   const starSize = 4 + Math.random() * 16;
   const tailHeight = Math.max(1, starSize / 10);
   const tailWidth = 15 * starSize;
-  const travelDistance = Math.max(window.innerWidth, window.innerHeight);
-
-  // Random starting position near the edges of the viewport
-  const top = Math.random() * window.innerHeight;
-  const left = Math.random() * window.innerWidth;
+  const angle = calculateRandomAngleTowardsCenter(left, top);
+  // Get the maximum possible travel distance. The intersection observer will remove the shooting star once it exits the viewport
+  const maxTravelDistance = Math.sqrt(
+    Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2),
+  );
+  // Duration increases as the viewport increases increases to prevent the shooting star from traveling too fast
+  const animationDuration = 2.5 + maxTravelDistance * 0.0007;
 
   return {
     id: Math.random(),
     top: `${top}px`,
     left: `${left}px`,
     size: `${starSize}px`,
-    rotation: calculateRandomAngleTowardsCenter(left, top),
+    rotation: `${angle}deg`,
     tailHeight: `${tailHeight}px`,
     tailWidth: `${tailWidth}px`,
-    travelDistance: `-${travelDistance}px`,
+    travelDistance: `-${maxTravelDistance}px`,
+    animationDuration: `${animationDuration}s`,
   };
 }
 
 export default function ShootingStar() {
-  const starRef = useRef<HTMLSpanElement | null>(null);
   const [shootingStars, setShootingStars] = useState<ShootingStar[]>([]);
 
   useEffect(() => {
@@ -74,7 +79,6 @@ export default function ShootingStar() {
   }, []);
 
   async function handleAnimationEnd() {
-    setShootingStars([]);
     await wait(4000);
     setShootingStars([createShootingStar()]);
   }
@@ -82,25 +86,68 @@ export default function ShootingStar() {
   return (
     <div className="fixed left-0 top-0 -z-50 h-full w-full overflow-hidden">
       {shootingStars.map((shootingStar) => (
-        <span
+        <ShootingStarItem
           key={shootingStar.id}
-          ref={starRef}
-          style={
-            {
-              top: shootingStar.top,
-              left: shootingStar.left,
-              width: shootingStar.size,
-              height: shootingStar.size,
-              "--shooting-star-rotation": shootingStar.rotation,
-              "--shooting-star-tail-height": shootingStar.tailHeight,
-              "--shooting-star-tail-width": shootingStar.tailWidth,
-              "--shooting-star-travel-distance": shootingStar.travelDistance,
-            } as React.CSSProperties
-          }
-          className="shooting-star"
-          onAnimationEnd={handleAnimationEnd}
+          shootingStar={shootingStar}
+          onLeaveViewport={handleAnimationEnd}
         />
       ))}
     </div>
+  );
+}
+
+type ShootingStarItemProps = {
+  shootingStar: ShootingStar;
+  onLeaveViewport: () => void;
+};
+
+function ShootingStarItem({
+  shootingStar,
+  onLeaveViewport,
+}: ShootingStarItemProps) {
+  const shootingStarRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const currentStar = shootingStarRef.current;
+
+    // Use IntersectionObserver to remove shooting star once it exits the viewport
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) {
+          onLeaveViewport();
+        }
+      },
+      { threshold: 0 }, // Trigger when the element is no longer visible at all.
+    );
+
+    if (currentStar) {
+      observer.observe(currentStar);
+    }
+
+    return () => {
+      if (currentStar) {
+        observer.unobserve(currentStar);
+      }
+    };
+  }, [onLeaveViewport, shootingStar.animationDuration]);
+
+  return (
+    <span
+      ref={shootingStarRef}
+      style={
+        {
+          top: shootingStar.top,
+          left: shootingStar.left,
+          width: shootingStar.size,
+          height: shootingStar.size,
+          "--shooting-star-rotation": shootingStar.rotation,
+          "--shooting-star-tail-height": shootingStar.tailHeight,
+          "--shooting-star-tail-width": shootingStar.tailWidth,
+          "--shooting-star-travel-distance": shootingStar.travelDistance,
+          "--shooting-star-animation-duration": shootingStar.animationDuration,
+        } as React.CSSProperties
+      }
+      className="shooting-star"
+    />
   );
 }
