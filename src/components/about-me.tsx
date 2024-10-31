967a92d64b920f7373118edd53bf37b738ad8e86
GraydonvG <graydonvg@gmail.com>
2024-10-31 07:55:59 +0200

    set color scheme to dark

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -5,11 +5,11 @@ import TypographyP from "./ui/typography/p";
 
 export default function AboutMe() {
   return (
-    <div className="hole-container grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 xl:gap-16">
-      <div className="bg-spaceBackground hidden aspect-square w-full rounded-2xl md:block"></div>
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 xl:gap-16">
+      <div className="hidden aspect-square w-full rounded-2xl bg-spaceBackground md:block"></div>
       <div className="flex flex-col items-center justify-center gap-6 md:items-start">
         <TypographyH2>About me</TypographyH2>
-        <div className="bg-spaceBackground block aspect-square w-full rounded-2xl md:hidden"></div>
+        <div className="block aspect-square w-full rounded-2xl bg-spaceBackground md:hidden"></div>
         <div>
           <TypographyP>
             I started my career in hospitality as a restaurant manager, but I
