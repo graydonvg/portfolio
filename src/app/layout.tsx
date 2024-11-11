be69ede7c54838be699382e1552fe587cb411407
GraydonvG <graydonvg@gmail.com>
2024-11-11 13:53:15 +0200

    update metadata, add viewport config to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,31 @@ import { ReactLenis } from "lenis/react";
 import Providers from "./providers";
 import Preloader from "@/components/preloader/preloader";
 import ogImage from "../../public/opengraph-image.png";
+import type { Viewport } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-const description =
-  "Graydon von Gossler's portfolio showcasing front-end development skills including but not limited to JavaScript, TypeScript, React.js, Next.js, and Tailwind CSS.";
+export const viewport: Viewport = {
+  themeColor: "black",
+  colorScheme: "dark light",
+};
+
+const NAME = "Graydon von Gossler";
+const TITLE = `${NAME} | Web Developer`;
+const DESCRIPTION =
+  "Portfolio showcasing projects and web development skills in JavaScript, TypeScript, React.js, Next.js, Tailwind CSS, and more.";
 
 export const metadata: Metadata = {
-  title: "Graydon von Gossler",
-  description,
+  title: TITLE,
+  description: DESCRIPTION,
+  applicationName: `${NAME} - Portfolio`,
+  generator: "Next.js",
+  authors: [{ name: NAME }],
+  creator: NAME,
+  publisher: NAME,
   keywords:
     "portfolio, front-end developer, frontend engineer, web development, web developer, frontend developer, creative, problem-solving, team player, UI/UX, user interface, javascript developer, typescript developer, react developer, nextjs developer, next.js app router, html, css, javascript, typescript, reactjs, react.js, redux, nextjs, next.js, tailwind, threejs, three.js, 3D graphics, 3D animations, framer motion, supabase, firebase, mui, shadcn, parallax, freelance, remote, startup-friendly, responsive design, high-performance websites, modern web design, english, german, afrikaans, south africa, international, graydon von gossler",
   icons: [
@@ -55,14 +68,15 @@ export const metadata: Metadata = {
     },
   ],
   openGraph: {
-    title: "Graydon von Gossler",
-    description,
+    title: TITLE,
+    description: DESCRIPTION,
+    siteName: NAME,
     type: "website",
     url: "https://portfolio-iota-ruby-51.vercel.app/",
     images: [
       {
         url: ogImage.src,
-        alt: "Graydon von Gossler's portfolio open graph image",
+        alt: `${NAME} - Web Developer`,
         width: ogImage.width,
         height: ogImage.height,
       },
@@ -70,16 +84,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Graydon von Gossler",
-    description,
-    images: [
-      {
-        url: ogImage.src,
-        alt: "Graydon von Gossler's portfolio open graph image",
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
+    title: TITLE,
+    description: DESCRIPTION,
+    images: [ogImage.src],
   },
 };
 
