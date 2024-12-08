195cd9dd54c0a01c0113e2e4ff93cf950a1b57b8
GraydonvG <graydonvg@gmail.com>
2024-12-08 10:47:07 +0200

    update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      disallow: "/docs/resume.pdf",
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
